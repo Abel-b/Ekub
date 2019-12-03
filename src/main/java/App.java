5a3789056720916835f0db4d55e9d4003ee3d8fe
Keith Omware <keithomware@gmail.com>
2019-12-03 11:27:46 +0300

    Create templates and images folder

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,32 @@
+import spark.ModelAndView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
 
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
     }
 }
