096bbb826d923d51438770c5bdf875bba3702e8c
Keith Omware <keithomware@gmail.com>
2019-12-03 12:11:17 +0300

    Create signupform.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,6 +27,11 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+        get("/signin", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "Signinform.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
     }
 }
