c0180edb158636576e1a935d60f8c544212cf355
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-12-05 08:12:48 +0300

    Add getters && setters to  User pojo

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -7,15 +7,16 @@ import java.util.List;
 public class User {
     private int id;
     private String username;
-    private String password;
     private String email;
     private int phonenumber;
+    private String password;
 
-    public User(String username, String password, String email, int phonenumber) {
-        this.username=username;
-        this.password=password;
-        this.email=email;
-        this.phonenumber=phonenumber;
+
+    public User(String username, String email, int phonenumber, String password) {
+        this.username = username;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.password = password;
     }
 
     public int getId() {
@@ -38,5 +39,23 @@ public class User {
         return email;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
 }
